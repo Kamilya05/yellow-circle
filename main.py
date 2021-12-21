ed3885d211e803aae084de5ab2b3624d7df163fb
Kamilya05 <94891282+Kamilya05@users.noreply.github.com>
2021-12-22 00:56:53 +0500

    Git и случайные окружности

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import sys
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from random import randint
 
-Ui_Form, QMainWindowWindow = uic.loadUiType("UI.ui")
 
-
-class MyWidget(QMainWindow, Ui_Form):
+class Form(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle('Git и желтые окружности')
+        self.setGeometry(200, 200, 600, 500)
+        self.pushButton = QPushButton('Больше окружностей!!!', self)
+        self.pushButton.setGeometry(225, 225, 150, 50)
+
+
+class MyWidget(Form):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Git и случайные окружности')
         self.coords = []
         self.pushButton.clicked.connect(self.click)
 
     def click(self):
         x, y = randint(0, 600), randint(0, 500)
         d = randint(5, 150)
-        self.coords.append((x, y, d))
+        c = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.coords.append((x, y, d, c))
         self.update()
 
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.begin(self)
-        qp.setBrush(QColor('yellow'))
-        for x, y, d in self.coords:
+        for x, y, d, c in self.coords:
+            qp.setBrush(c)
             qp.drawEllipse(x, y, d, d)
         qp.end()
 
